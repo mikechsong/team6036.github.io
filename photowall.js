bd49a9198e5a1dd85764f1331838021c90f94bdf
Kyle Heller <kyleeheller@gmail.com>
2019-02-05 17:15:55 -0800

    Set photowall.js just to see what happens

--- photowall.js
+++ photowall.js
@@ -1,1 +1,22 @@
-
+function photoloader(){
+  var xhr = new XMLHttpRequest(); 
+xhr.open("GET", "https://photoslibrary.googleapis.com/v1/albums", true); 
+xhr.onload = function (e) { 
+if (xhr.readyState === 4) { 
+if (xhr.status === 200) { 
+var respJSON = JSON.parse(xhr.responseText); 
+alert(respJSON.Accounts.length); 
+} else { 
+//console.error(xhr.statusText); 
+//handle error status 
+} 
+} 
+}; 
+xhr.onerror = function (e) { 
+console.error(xhr.statusText); 
+}; 
+xhr.setRequestHeader("Authorization", "Bearer 721616495193-dnga1n0ggro6efodmsephuos1065l0h5.apps.googleusercontent.com")
+xhr.send(null); 
+  
+  
+}
