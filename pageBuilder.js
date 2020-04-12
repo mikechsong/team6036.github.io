88aece934f07d7c4efbd4f23f3ac3a83907bcd8e
Neben5 <jlsatpa@gmail.com>
2020-04-12 16:39:44 -0700

    attempt failed, retrying

--- pageBuilder.js
+++ pageBuilder.js
@@ -6,7 +6,10 @@ function nav(inputURL) {
         for (x in data) {
             output += "<li class=\"nav-item\">\n";
             output += "<a class=\"nav-link\" href=\"" + data[x].link + "\">" + data[x].name
-            console.log(window.location.href);
+            console.log("https://" + data[x].link + "/");
+            if ("https://" + data[x].link + "/" === window.location.href) {
+                console.log("HI");
+            }
             if (window.location.href === "https://team6036.github.io/" && "https://" + data[x].link + "/" === window.location.href) {
                 // if this is root page, and it is root link, active
                 output += "<span class=\"sr-only\">(current)</span>"
