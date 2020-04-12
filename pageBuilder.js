1739b82ee89d52b9ab545f26b129aab5574d65f5
Neben5 <jlsatpa@gmail.com>
2020-04-12 16:14:23 -0700

    finalized json reading, seeing how I can make page loading seem more dynamic

--- pageBuilder.js
+++ pageBuilder.js
@@ -1,31 +1,18 @@
-function getText(url) {
-    // read text from URL location
-    var request = new XMLHttpRequest();
-    request.open('GET', url, true);
-    request.send(null);
-    request.onreadystatechange = function () {
-        if (request.readyState === 4 && request.status === 200) {
-            var type = request.getResponseHeader('Content-Type');
-            if (type.indexOf("text") !== 1) {
-                return request.responseText;
-            }
-        }
-    }
-}
 
 // Navbar
 function nav(inputURL) {
-    var inputJSON = JSON.parse(getText(inputURL));
-
-    var output = "";
-    for (x in inputJSON) {
-        console.log(inputJSON);
-        console.log()
-        output += "<li class=\"nav-item\">\n";
-        output += "<a class=\"nav-link\" href=\"" + inputJSON[x].link + "\">" + inputJSON[x].name + "</a>";
-        output += "</li>";
-    }
-
-    return output;
+        $.getJSON(inputURL,function(data){
+            var output = "";
+            for (x in data) {
+                output += "<li class=\"nav-item\">\n";
+                output += "<a class=\"nav-link\" href=\"" + data[x].link + "\">" + data[x].name
+                if(data[x].link===window.location.href){
+                    output+="<span class=\"sr-only\">(current)</span>"
+                }
+                output+="</a>";
+                output += "</li>";
+            }
+            document.getElementById("nav").innerHTML=output;
+        });
 }
 document.getElementById("nav").innerHTML = nav("https://team6036.github.io/nav.json");
