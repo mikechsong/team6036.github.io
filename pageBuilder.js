8dcffee0b928e8105a411514a7e0a6123db170ef
Neben5 <jlsatpa@gmail.com>
2020-04-12 16:55:11 -0700

    think i finished 'dynamic' highlighting

--- pageBuilder.js
+++ pageBuilder.js
@@ -4,21 +4,14 @@ function nav(inputURL) {
     $.getJSON(inputURL, function (data) {
         var output = "";
         for (x in data) {
-            output += "<li class=\"nav-item\">\n";
-            output += "<a class=\"nav-link\" href=\"" + data[x].link + "\">" + data[x].name
-            console.log(data[x].link + "/");
-            if (data[x].link + "/" === window.location.href) {
-                console.log("HI");
-            }
-            if (window.location.href === "https://team6036.github.io/" && data[x].link + "/" === window.location.href) {
-                // if this is root page, and it is root link, active
-                output += "<span class=\"sr-only\">(current)</span>"
-            } else if (data[x].link === window.location.href) {
+
+            if (data[x].link === window.location.href || data[x].link + "/" === window.location.href) {
                 //if it is not root page, but right link, make active
-                output += "<span class=\"sr-only\">(current)</span>"
+                output += "<li class=\"nav-item active\"><a class=\"nav-link\" href=\"" + data[x].link + "\">" + data[x].name + "<span class=\"sr-only\">(current)</span>";
+            } else {
+                output += "<li class=\"nav-item \"><a class=\"nav-link\" href=\"" + data[x].link + "\">" + data[x].name
             }
-            output += "</a>";
-            output += "</li>";
+            output += "</a></li>";
         }
         document.getElementById("nav").innerHTML = output;
     });
